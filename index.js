a0a516c195ffc041602cee07c21ad7947ccc6d8e
Mugara250 <mbonyumugara52@gmail.com>
2025-04-10 22:19:40 +0200

    add Enter key event

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-/* 
-1. Create an element called "li"
-2. Capture the content of the input element
-3. Set that content as the text content of the "li" element
-4. Add the "li" element to the end of the tasks element
-*/
-
 // selecting the needed buttons
 const addTaskButton = document.querySelector(".add-button");
 const inputElement = document.querySelector("#task");
 const tasksList = document.querySelector(".tasks");
 
-
-// task creation and addition
-addTaskButton.addEventListener('click', function addTask(event) {
+// adding the keypress event when the "Enter" key is clicked
+inputElement.addEventListener("keydown", function taskAdd(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
     // task creation
     const inputText = inputElement.value;
     const task = document.createElement("li");
-    task.className = "added";
     task.style.display = "flex";
     task.style.justifyContent = "space-between";
     task.style.alignItems = "baseline";
@@ -27,7 +20,7 @@ addTaskButton.addEventListener('click', function addTask(event) {
     deleteButton.href = "#";
     deleteButton.className = "remove";
     deleteButton.textContent = "REMOVE";
-    deleteButton.style.backgroundColor = "#1789A3"
+    deleteButton.style.backgroundColor = "#1789A3";
     deleteButton.style.paddingTop = "0.75rem";
     deleteButton.style.paddingBottom = "0.75rem";
     deleteButton.style.paddingLeft = "1.25rem";
@@ -35,25 +28,52 @@ addTaskButton.addEventListener('click', function addTask(event) {
     deleteButton.style.fontWeight = "bold";
     deleteButton.style.borderRadius = "1rem";
 
-    // addition of the task and remove button 
+    // addition of the task and remove button
     task.appendChild(deleteButton);
     tasksList.appendChild(task);
     inputElement.value = "";
 
     // task removal
-    // const deleteButton = document.querySelector(".remove");
     deleteButton.addEventListener("click", function removeTask(event) {
-        event.preventDefault();
-        const task = document.querySelector(".added");
-        tasksList.removeChild(task);
-    })
-})
+      event.preventDefault();
+      const task = document.querySelector("li");
+      tasksList.removeChild(task);
+    });
+  }
+});
+// task creation and addition
+addTaskButton.addEventListener("click", function addTask(event) {
+  // task creation
+  const inputText = inputElement.value;
+  const task = document.createElement("li");
+  // task.className = "added";
+  task.style.display = "flex";
+  task.style.justifyContent = "space-between";
+  task.style.alignItems = "baseline";
+  task.textContent = inputText;
 
-// // task removal
-// const deleteButton = document.querySelector(".remove");
-// deleteButton.addEventListener("click", function removeTask(event) {
-//     event.preventDefault();
-//     const task = document.querySelector(".added");
-//     tasksList.removeChild(task);
-//     tasksList.removeChild(deleteButton);
-// })
+  // remove button creation
+  const deleteButton = document.createElement("a");
+  deleteButton.href = "#";
+  deleteButton.className = "remove";
+  deleteButton.textContent = "REMOVE";
+  deleteButton.style.backgroundColor = "#1789A3";
+  deleteButton.style.paddingTop = "0.75rem";
+  deleteButton.style.paddingBottom = "0.75rem";
+  deleteButton.style.paddingLeft = "1.25rem";
+  deleteButton.style.paddingRight = "1.25rem";
+  deleteButton.style.fontWeight = "bold";
+  deleteButton.style.borderRadius = "1rem";
+
+  // addition of the task and remove button
+  task.appendChild(deleteButton);
+  tasksList.appendChild(task);
+  inputElement.value = "";
+
+  // task removal
+  deleteButton.addEventListener("click", function removeTask(event) {
+    event.preventDefault();
+    const task = document.querySelector("li");
+    tasksList.removeChild(task);
+  });
+});
