6ab1ab65fc861f5e6b22e946e8d53cd9fdaab0f9
Mugara250 <mbonyumugara52@gmail.com>
2025-06-28 12:38:01 +0200

    Added functionality to the add button

--- index.js
+++ index.js
@@ -1,79 +1,11 @@
-// selecting the needed buttons
-const addTaskButton = document.querySelector(".add-button");
-const inputElement = document.querySelector("#task");
-const tasksList = document.querySelector(".tasks");
+// selecting the needed elements
+const userInput = document.querySelector("#task");
+const addButton = document.querySelector("#add-btn");
+const todos = document.querySelector("#todos");
 
-// adding the keypress event when the "Enter" key is clicked
-inputElement.addEventListener("keydown", function taskAdd(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    // task creation
-    const inputText = inputElement.value;
-    const task = document.createElement("li");
-    task.style.display = "flex";
-    task.style.justifyContent = "space-between";
-    task.style.alignItems = "baseline";
-    task.textContent = inputText;
-
-    // remove button creation
-    const deleteButton = document.createElement("a");
-    deleteButton.href = "#";
-    deleteButton.className = "remove";
-    deleteButton.textContent = "REMOVE";
-    deleteButton.style.backgroundColor = "#1789A3";
-    deleteButton.style.paddingTop = "0.75rem";
-    deleteButton.style.paddingBottom = "0.75rem";
-    deleteButton.style.paddingLeft = "1.25rem";
-    deleteButton.style.paddingRight = "1.25rem";
-    deleteButton.style.fontWeight = "bold";
-    deleteButton.style.borderRadius = "1rem";
-
-    // addition of the task and remove button
-    task.appendChild(deleteButton);
-    tasksList.appendChild(task);
-    inputElement.value = "";
-
-    // task removal
-    deleteButton.addEventListener("click", function removeTask(event) {
-      event.preventDefault();
-      const task = document.querySelector("li");
-      tasksList.removeChild(task);
-    });
-  }
-});
-// task creation and addition
-addTaskButton.addEventListener("click", function addTask(event) {
-  // task creation
-  const inputText = inputElement.value;
-  const task = document.createElement("li");
-  // task.className = "added";
-  task.style.display = "flex";
-  task.style.justifyContent = "space-between";
-  task.style.alignItems = "baseline";
-  task.textContent = inputText;
-
-  // remove button creation
-  const deleteButton = document.createElement("a");
-  deleteButton.href = "#";
-  deleteButton.className = "remove";
-  deleteButton.textContent = "REMOVE";
-  deleteButton.style.backgroundColor = "#1789A3";
-  deleteButton.style.paddingTop = "0.75rem";
-  deleteButton.style.paddingBottom = "0.75rem";
-  deleteButton.style.paddingLeft = "1.25rem";
-  deleteButton.style.paddingRight = "1.25rem";
-  deleteButton.style.fontWeight = "bold";
-  deleteButton.style.borderRadius = "1rem";
-
-  // addition of the task and remove button
-  task.appendChild(deleteButton);
-  tasksList.appendChild(task);
-  inputElement.value = "";
-
-  // task removal
-  deleteButton.addEventListener("click", function removeTask(event) {
-    event.preventDefault();
-    const task = document.querySelector("li");
-    tasksList.removeChild(task);
-  });
-});
+// adding the event listener to the add button
+addButton.addEventListener('click', () => {
+  const todo = document.createElement("li");
+  todo.innerHTML = `${userInput.value} <a href="#" id="remove-btn" ><button type="button" id="remove">REMOVE</button></a>`;
+  todos.appendChild(todo);
+})
